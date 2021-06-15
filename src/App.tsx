a3bcec3d045c6f6cdf95467e58abcbbd61a373d4
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-06-15 00:38:35 -0700

    Use React class components

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Work in progress...</p>
-        <a
-          className="App-link"
-          href="https://github.com/cooperuser/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My GitHub
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Work in progress...</p>
+          <a
+            className="App-link"
+            href="https://github.com/cooperuser/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My GitHub
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
