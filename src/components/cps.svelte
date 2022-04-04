1846cde92eb688725c4854815864e3b3482243bf
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-04-03 23:08:24 -0700

    Add stupid clicks per second gauge

---
+++ src/components/cps.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let clicks = 0;
+
+	function onClick() {
+		clicks++;
+		setTimeout(() => clicks--, 1000);
+	}
+</script>
+
+<main>
+	<h1>{clicks} clicks per second</h1>
+	<button on:click={onClick}>click</button>
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import Cps from "../components/cps.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="/">this page</a> to view my website. hi tom.</p>
+<Cps />
